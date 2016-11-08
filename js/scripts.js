57c39a74870fbc963f5032e57878f00d7e442a32
jjp1994 <jpayne13@vcd-ch204-22.eastern.ewu.edu>
2016-11-08 14:34:43 -0800

    version0

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// YOUR SCRIPTS
+$('#grid').click(function(){
+    
+  //remove Only list class//
+    $('.cd-gallery').removeClass('list');
+    
+//add Only grid class//
+    $('.cd-gallery').addClass('grid');
+    
+    
+})
+
+
+
+
+
+//LIST VIEW//
+
+$('#grid').click(function(){
+    
+  //remove Only list class//
+    $('.cd-gallery').removeClass('grid');
+    
+//add Only grid class//
+    $('.cd-gallery').addClass('list');
+    
+    
+    
+    
+    
+})
+
+
+
+
